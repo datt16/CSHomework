d60a67cee0d63164fd0be4b11f22d36e3aaf9879
datt16 <osatom.4639@gmail.com>
2020-01-03 02:25:37 +0900

    class作成、ignore修正

---
+++ Jadge/Classes/Match.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Judge.Classes {
+    class Match {
+        public string GenerateCountText(int CountA,int CountB) {
+            if (CountA >= 10) {
+                return CountA.ToString() + " - 0" + CountB.ToString();
+            }else if(CountB >= 10) {
+                return "0" + CountA.ToString() + " - " + CountB.ToString();
+            }
+            return CountA.ToString() + " - " + CountB.ToString();
+        }
+    }
+}
