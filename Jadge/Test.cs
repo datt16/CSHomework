f8383515b13d664978d1cf0847580a835dd5569a
datt16 <osatom.4639@gmail.com>
2020-01-03 01:18:08 +0900

    除外設定の変更

---
+++ Jadge/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jadge {
+    class Test {
+        public string Hello() {
+            return "Hello";
+        }
+    }
+}
