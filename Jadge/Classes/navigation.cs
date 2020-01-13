e908e7df7d980ba40e50f3cc00be5359a5fa3d28
datt16 <osatom.4639@gmail.com>
2020-01-13 22:10:45 +0900

    ナビゲーションの準備

---
+++ Jadge/Classes/navigation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Judge.Classes {
+    public class Navigation {
+        public static Type GetNavigateTo(NavigationViewItem item) {
+            return (Type)item.GetValue(NavigateToProperty);
+        }
+
+        public static void SetNavigateTo(NavigationViewItem item, Type value) {
+            item.SetValue(NavigateToProperty, value);
+        }
+
+        public static readonly DependencyProperty NavigateToProperty =
+            DependencyProperty.RegisterAttached("NavigateTo", typeof(Type), typeof(Navigation), new PropertyMetadata(null));
+    }
+}
