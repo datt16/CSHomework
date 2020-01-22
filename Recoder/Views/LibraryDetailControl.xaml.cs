f8f65dcb45d2fcd660468252f903e7e52a1fa4fc
datt16 <osatom.4639@gmail.com>
2020-01-23 00:12:30 +0900

    1st commit Windows TempLate Studioで作成したアプリを若干改変

---
+++ Recoder/Views/LibraryDetailControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Recoder.Core.Models;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Recoder.Views
+{
+    public sealed partial class LibraryDetailControl : UserControl
+    {
+        public SampleOrder MasterMenuItem
+        {
+            get { return GetValue(MasterMenuItemProperty) as SampleOrder; }
+            set { SetValue(MasterMenuItemProperty, value); }
+        }
+
+        public static readonly DependencyProperty MasterMenuItemProperty = DependencyProperty.Register("MasterMenuItem", typeof(SampleOrder), typeof(LibraryDetailControl), new PropertyMetadata(null, OnMasterMenuItemPropertyChanged));
+
+        public LibraryDetailControl()
+        {
+            InitializeComponent();
+        }
+
+        private static void OnMasterMenuItemPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as LibraryDetailControl;
+            control.ForegroundElement.ChangeView(0, 0, 1);
+        }
+    }
+}
---
+++ Recoder/Views/MainDetailPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+using Microsoft.Toolkit.Uwp.UI.Animations;
+
+using Recoder.Core.Models;
+using Recoder.Core.Services;
+using Recoder.Services;
+
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Recoder.Views
+{
+    public sealed partial class MainDetailPage : Page, INotifyPropertyChanged
+    {
+        private SampleOrder _item;
+
+        public SampleOrder Item
+        {
+            get { return _item; }
+            set { Set(ref _item, value); }
+        }
+
+        public MainDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            if (e.Parameter is long orderID)
+            {
+                var data = await SampleDataService.GetContentGridDataAsync();
+                Item = data.First(i => i.OrderID == orderID);
+            }
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            if (e.NavigationMode == NavigationMode.Back)
+            {
+                NavigationService.Frame.SetListDataItemForNextConnectedAnimation(Item);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
