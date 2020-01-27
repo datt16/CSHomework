f75c39b02ca3c86313e62e21784d6e910e46bce2
datt16 <osatom.4639@gmail.com>
2020-01-28 00:06:20 +0900

    ラリーの回数を記録できるようにした

--- Recoder.Core/Models/Point.cs
+++ Recoder.Core/Models/Point.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 
 namespace Recoder.Core.Models {
+
     public class Point {
+
         public int Id { get; set; }
+
         public string Getter { get; set; }
+
+        public int Rally { get; set; } = 0;
+
         public ICollection<Tag> Tags { get; set; }
     }
 }
