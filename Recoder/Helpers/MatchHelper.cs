1eac82e84e25f38fb479cbdb713d4f2a9933fbdd
datt16 <osatom.4639@gmail.com>
2020-01-28 00:05:36 +0900

    デバッグ用のメソッドを追加

--- Recoder/Helpers/MatchHelper.cs
+++ Recoder/Helpers/MatchHelper.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
 using Recoder.Core.Models;
 
 namespace Recoder.Helpers {
-    class MatchHelper {
-        
+    public class MatchHelper {
+
+        public TextBlock tagText = new TextBlock()
+        {
+            Text = "",
+            Margin = new Thickness(5, 10, 10, 10),
+            FontSize = 12
+        };
+
         public string GenerateCountText(int CountA, int CountB, string type = "SoftTennis") {
             if (type == "Base") {
                 if (CountA >= 10) {
@@ -35,5 +48,17 @@ namespace Recoder.Helpers {
             }
             return CountA.ToString() + " - " + CountB.ToString();
         }
+        public void Debug_ShowTag(List<Tag> tags) {
+            StringBuilder res = new StringBuilder();
+            int i = 0;
+            res.Append("----) Debug_ShowTag (----\n");
+            foreach (Tag tag in tags) {
+                res.Append($"{i} : {tag.TagName}\n");
+                i++;
+            }
+            res.Append("-------------------------");
+            Debug.WriteLine($"{res}");
+        }
     }
+
 }
