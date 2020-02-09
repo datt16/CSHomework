ffcb4d2151801198039c74b21b5a2182dd15474d
datt16 <osatom.4639@gmail.com>
2020-02-09 11:59:03 +0900

    ホーム画面の実装

--- Recoder/Views/MainPage.xaml.cs
+++ Recoder/Views/MainPage.xaml.cs
@@ -1,16 +1,48 @@
-﻿using System;
+﻿using Recoder.Core.Models;
+using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 namespace Recoder.Views
 {
     public sealed partial class MainPage : Page, INotifyPropertyChanged
     {
+        private ObservableCollection<MatchData> _matches = new ObservableCollection<MatchData>();
+
+        public ObservableCollection<MatchData> matches {
+            get { return this._matches; }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e) {
+            base.OnNavigatedTo(e);
+            matches.Add(
+                new MatchData
+                {
+                    TeamAName = "TeamA",
+                    TeamBName = "TeamB",
+                    GamesCount = 5,
+                    TeamA_GamePoint = 1,
+                    TeamB_GamePoint = 3,
+                });
+            matches.Add(
+                new MatchData
+                {
+                    TeamAName = "Apple",
+                    TeamBName = "Microsoft",
+                    GamesCount = 7,
+                    TeamA_GamePoint = 4,
+                    TeamB_GamePoint = 3,
+                });
+        }
+
         public MainPage()
         {
             InitializeComponent();
+            
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
